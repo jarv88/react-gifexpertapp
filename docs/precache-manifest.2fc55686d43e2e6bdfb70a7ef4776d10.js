0fcfa7ccb376102a66a2f184d885ae390ea7bdf7
jarv88 <joseangel038@gmail.com>
2020-10-16 12:42:07 -0500

    Docs agregado

---
+++ docs/precache-manifest.2fc55686d43e2e6bdfb70a7ef4776d10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3178e9dbd2987cea330b9be04cde4bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5154305843a2b0ccc67a",
+    "url": "/static/css/main.95a0dd58.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5154305843a2b0ccc67a",
+    "url": "/static/js/main.82eeadf1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
